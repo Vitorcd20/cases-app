d8933354459f60a5ad4c306b429f212d27f5c347
unknown <vitorvieeira12008@gmai.com>
2024-10-09 18:33:01 -0300

    initial commit

--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -1,7 +1,6 @@
 // bg-blue-950 border-blue-950
 // bg-zinc-900 border-zinc-900
 // bg-rose-950 border-rose-950
-// bg-pink-950 border-pink-900
 
 import { PRODUCT_PRICES } from "@/config/products";
 
@@ -9,7 +8,6 @@ export const COLORS = [
   { label: "Black", value: "black", tw: "zinc-900" },
   { label: "Blue", value: "blue", tw: "blue-950" },
   { label: "Rose", value: "rose", tw: "rose-950" },
-  { label: "Pink", value: "pink", tw: "pink-900" },
 ] as const;
 
 export const MODELS = {
